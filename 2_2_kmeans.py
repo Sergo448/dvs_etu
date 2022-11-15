f61c3e363db7349aae5e229952cc1337909ffbfc
Sergo448 <Sergey.6362@mail.ru>
2022-11-15 11:13:46 +0300

    Initial commit

---
+++ 2_2_kmeans.py
@@ -0,0 +1,10 @@
+# project name: dvs_etu
+# version: 1.0
+# file name: 2_2_kmeans.py
+# auther: Sergo448, 7193
+# date: 15.11.2021
+# libs: numpy, sklearn, opencv
+# Python version : 3.10
+
+import cv2
+
