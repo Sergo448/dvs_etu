8a3f7b565b3a968232e419c1e6287755f5bb1c0b
Sergo448 <Sergey.6362@mail.ru>
2022-12-07 16:02:06 +0300

    first part of remaking regressor

--- regresor_RGB/regresor_2.py
+++ regresor_RGB/regresor_2.py
@@ -1,56 +1,151 @@
 import cv2 as cv
 import numpy as np
+import os
 
 
-class Regressor_RGB:
-    """
-    Реализуем класс регрессора для изображений формата RGB
-    для того, чтобы устранить хроматическую аберрацию на цветном
-    изображении.
-    """
+def writer(image_):
+    # Создание директории в которой будут храниться результаты
+    if not os.path.isdir("results"):
+        os.mkdir("results")
 
-    def __init__(self, image):
-        self.image = cv.imread(image, cv.IMREAD_COLOR)
+    save_path = 'results'
 
-    # Пока откажемся от перевода в вектор
-    """
-   def load_image(self):
-        img_array = np.asarray(self.image, dtype="int32")
-        # print(img_array)
-        return img_array
-    """
+    print(f'Запись результата работы модели')
 
-    def make_vector_and_split(self):
-        """
-        Векторизируем изображение
-        Создаем 3 различных вектора из исходного
-        трансформируем изображение (объединяем)
+    path_ = os.path.join(save_path, 'result_.jpg')
+    cv.imwrite(path_, image_)
 
-        :return: transformed_image_by_merge, transformed_image_by_weights
 
-        """
-        R_image, G_image, B_image = cv.split(self.image)
-        """
-        R_image = img_arr[2].copy()
-        G_image = img_arr[1].copy()
-        B_image = img_arr[0].copy()
+# "Панорама" для красного и зеленого оттенка
+def panorams_R_G(img_, img):
+    img1 = cv.cvtColor(img_, cv.COLOR_BGR2GRAY)
 
-        # print(R_image)
-        # print(B_image)
-        # print(G_image)
-        """
+    img2 = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-        # merge the transformed channels back to an image
-        _merge = cv.merge((R_image, G_image, B_image))
+    shift = cv.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = shift.detectAndCompute(img1, None)
+    kp2, des2 = shift.detectAndCompute(img2, None)
 
-        transformed_image_by_merge = cv.addWeighted(self.image, 0.75, _merge, 0.05, 0)
-        # В данный момент пока не отлажено
-        """
-        alpha = 0.25
-        beta = 0.75
-        transformed_image_by_weights = cv.addWeighted(self.image, alpha, transformed_image_by_merge, beta, 0.0)
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
 
-        return transformed_image_by_merge, transformed_image_by_weights
-        """
-        return transformed_image_by_merge
-        # return _merge
+    # Apply ratio test
+    good = []
+    for m in matches:
+        if m[0].distance < 0.5 * m[1].distance:
+            good.append(m)
+        matches = np.asarray(good)
+
+    if len(matches[:, 0]) >= 4:
+        src = np.float32([kp1[m.queryIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        dst = np.float32([kp2[m.trainIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        H, masked = cv.findHomography(src, dst, cv.RANSAC, 5.0)
+    # print H
+    else:
+        raise AssertionError('Can’t find enough keypoints.')
+
+    dst = cv.warpPerspective(img_, H, (img.shape[1] + img_.shape[1], img.shape[0]))
+
+    dst[0: img.shape[0], 0: img.shape[1]] = img
+    cv.imwrite('result_image_for_R_G.jpg', dst)
+    result_ = dst
+
+    return result_
+
+
+# "Панорама" для синего и зеленого оттенка
+def panorams_B_G(img_, img):
+    img1 = cv.cvtColor(img_, cv.COLOR_BGR2GRAY)
+
+    img2 = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    shift = cv.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = shift.detectAndCompute(img1, None)
+    kp2, des2 = shift.detectAndCompute(img2, None)
+
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    # Apply ratio test
+    good = []
+    for m in matches:
+        if m[0].distance < 0.5 * m[1].distance:
+            good.append(m)
+        matches = np.asarray(good)
+
+    if len(matches[:, 0]) >= 4:
+        src = np.float32([kp1[m.queryIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        dst = np.float32([kp2[m.trainIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        H, masked = cv.findHomography(src, dst, cv.RANSAC, 5.0)
+    # print H
+    else:
+        raise AssertionError('Can’t find enough keypoints.')
+
+    dst = cv.warpPerspective(img_, H, (img.shape[1] + img_.shape[1], img.shape[0]))
+
+    dst[0: img.shape[0], 0: img.shape[1]] = img
+    cv.imwrite('result_image_for_B_G.jpg', dst)
+
+
+# "Панорама" приводящая все к уровню зеленого
+def panorams_RGB(img_, img):
+    img1 = cv.cvtColor(img_, cv.COLOR_BGR2GRAY)
+
+    img2 = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    shift = cv.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = shift.detectAndCompute(img1, None)
+    kp2, des2 = shift.detectAndCompute(img2, None)
+
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    # Apply ratio test
+    good = []
+    for m in matches:
+        if m[0].distance < 0.5 * m[1].distance:
+            good.append(m)
+        matches = np.asarray(good)
+
+    if len(matches[:, 0]) >= 4:
+        src = np.float32([kp1[m.queryIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        dst = np.float32([kp2[m.trainIdx].pt for m in matches[:, 0]]).reshape(-1, 1, 2)
+        H, masked = cv.findHomography(src, dst, cv.RANSAC, 5.0)
+    # print H
+    else:
+        raise AssertionError('Can’t find enough keypoints.')
+
+    dst = cv.warpPerspective(img_, H, (img.shape[1] + img_.shape[1], img.shape[0]))
+
+    dst[0: img.shape[0], 0: img.shape[1]] = img
+    cv.imwrite('result_image_for_RGB.jpg', dst)
+
+
+def main(path_to_image):
+    image = cv.imread(path_to_image, cv.IMREAD_COLOR)
+
+    R_image, G_image, B_image = cv.split(image)
+
+    # Сохраняем оттенки изображений
+    cv.imwrite('R_image.jpg', R_image)
+    cv.imwrite('G_image.jpg', G_image)
+    cv.imwrite('B_image.jpg', B_image)
+
+    # Проведем склеивание панорамы красного изображения и зеленого.
+    panorams_R_G(img=cv.imread('G_image.jpg'), img_=cv.imread('R_image.jpg'))
+    panorams_B_G(img=cv.imread('G_image.jpg'), img_=cv.imread('B_image.jpg'))
+
+    panorams_RGB(img=cv.imread('result_image_for_R_G.jpg'), img_=cv.imread('result_image_for_B_G.jpg'))
+
+    res = panorams_RGB(img=cv.imread('result_image_for_R_G.jpg'), img_=cv.imread('result_image_for_B_G.jpg'))
+
+    writer(image_=res)
+
+
+path_to_image = r'.//images//image_with_aberation_2.jpg'
+
+if __name__ == "__main__":
+    main(path_to_image=path_to_image)
